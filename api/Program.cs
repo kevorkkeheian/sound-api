c7725521a7da8df1286c845e05d0225821979b5c
kevorkkeheian <kevorkkeheian@gmail.com>
2022-06-27 21:29:15 +0300

    aws: add credentials to app settings

--- api/Program.cs
+++ api/Program.cs
@@ -1,6 +1,8 @@
+using Amazon;
 using Amazon.DynamoDBv2;
 using Amazon.DynamoDBv2.DataModel;
 using Amazon.Extensions.NETCore.Setup;
+using Amazon.Runtime;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.Extensions.DependencyInjection;
 
@@ -12,12 +14,27 @@ builder.Services.AddDbContext<ApplicationDbContext>(options =>
     options.UseSqlServer(builder.Configuration.GetConnectionString("ApplicationDbContext") ?? 
                                                             throw new InvalidOperationException("Connection string 'ApplicationDbContext' not found.")));
 
+
+
+var awsAccessKey = builder.Configuration.GetValue<string>("AWS:Key");
+var awsAccessSecret = builder.Configuration.GetValue<string>("AWS:Secret");
+
+var credentials = new BasicAWSCredentials(awsAccessKey, awsAccessSecret);
+
+var config = new AmazonDynamoDBConfig()
+{
+    RegionEndpoint = RegionEndpoint.USEast1
+};
+
+var client = new AmazonDynamoDBClient(credentials, config);
+
 // Get the AWS profile information from configuration providers
 AWSOptions awsOptions = builder.Configuration.GetAWSOptions();
 
 // Configure AWS service clients to use these credentials
 builder.Services.AddDefaultAWSOptions(awsOptions);
-builder.Services.AddAWSService<IAmazonDynamoDB>();
+// builder.Services.AddAWSService<IAmazonDynamoDB>();
+builder.Services.AddSingleton<IAmazonDynamoDB>(client);
 builder.Services.AddScoped<IDynamoDBContext, DynamoDBContext>();
 
 
