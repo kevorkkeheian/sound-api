03af3faace20c2372332dbe503df2d3c87d5396f
kevorkkeheian <kevorkkeheian@gmail.com>
2022-07-11 23:11:07 +0300

    model: update json property names in sound model

--- api/Models/Sound.cs
+++ api/Models/Sound.cs
@@ -1,5 +1,6 @@
 using System.ComponentModel.DataAnnotations;
 using System.ComponentModel.DataAnnotations.Schema;
+using System.Text.Json.Serialization;
 using Amazon.DynamoDBv2.DataModel;
 using Amazon.DynamoDBv2.DocumentModel;
 using Application.Converter;
@@ -9,22 +10,23 @@ namespace Application.Models
 {
     public class Sound
     {
-        [JsonProperty("_id")]
+        [JsonPropertyName("_id")]
         public string Id { get; set; }
         
+        // [JsonPropertyName("ts")]
         [JsonProperty("ts")]
         public string TimeStamp { get; set; }
         
-        [JsonProperty("level")]
+        [JsonPropertyName("level")]
         public string Level { get; set; }
 
-        [DynamoDBProperty("loudness")]
+        [JsonPropertyName("loudness")]
         public decimal Loudness { get; set; }
 
-        [DynamoDBProperty("MacAddress")]
+        [JsonPropertyName("MacAddress")]
         public string MacAddress { get; set; }
 
-        [DynamoDBProperty("_event_time")]
+        [JsonPropertyName("_event_time")]
         public string CreatedOn { get; set; }
 
     }
