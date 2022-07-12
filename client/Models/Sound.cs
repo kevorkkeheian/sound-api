1fafd63b80d170bca48bf5c4805b281ec4531fd4
kevorkkeheian <kevorkkeheian@gmail.com>
2022-07-12 15:45:55 +0300

    model: update sound properties

--- client/Models/Sound.cs
+++ client/Models/Sound.cs
@@ -5,11 +5,11 @@ namespace Application.Models
 {
     public class Sound
     {
-        public Int64 TimeStamp { get; set; }
+        public string TimeStamp { get; set; }
         
         public string Level { get; set; }
 
-        public int Loudness { get; set; }
+        public decimal Loudness { get; set; }
 
         public string MacAddress { get; set; }
         public string CreatedOn { get; set; }
